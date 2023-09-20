4d13a141cfaa0144f1e4a5fcd34b58150c6e4ce8
DjordjeBl <djblagojevicpravni@gmail.com>
2023-09-20 18:15:11 +0200

    removed some unused imports, and refactored addViewControllers to include both home view and login view

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -1,17 +1,10 @@
 package com.example.demo.security;
 
-
-import static org.springframework.security.config.Customizer.withDefaults;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-
-
 
 @Configuration
 @EnableWebSecurity
@@ -22,19 +15,6 @@ public class SecurityConfig {
     return new BCryptPasswordEncoder();
   }
 
-  // TODO: 20-Sep-23  
-//  @Bean
-//  public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-//    http
-//        .authorizeRequests(authorizeRequests -> {
-//          authorizeRequests
-//              .antMatchers("/design", "/orders").hasRole("USER")
-//              .antMatchers("/", "/**").permitAll();
-//        })
-//        .formLogin(withDefaults())
-//        .loginPage("/login");
-//
-//    return http.build();
-//  }
+
 
 }
